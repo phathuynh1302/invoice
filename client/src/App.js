22971872aa1402abcb2b32cd96e4624318a53643
QuangHung <duongquanghung0122@gmail.com>
2023-02-07 10:00:36 +0700

    [HUNGDQSE150123] Config main

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./views/Homepage";
+import InvoiceHistory from "./views/InvoiceHistory";
+import AdminPage from "./views/AdminPage";
+import ManageUser from "./views/ManageUser";
+import MyAccount from "./views/MyAccount";
+import Auth from "./views/Auth";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/invoiceHistory" element={<InvoiceHistory />} />
+        <Route exact path="/adminPage" element={<AdminPage />} />
+        <Route exact path="/manageUser" element={<ManageUser />} />
+        <Route exact path="/myAccount" element={<MyAccount />} />
+        <Route exact path="/login" element={<Auth authRoute="login" />} />
+        <Route exact path="/register" element={<Auth authRoute="register" />} />
       </Routes>
     </Router>
   );
